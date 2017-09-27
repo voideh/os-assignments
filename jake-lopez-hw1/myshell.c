6575cd7d1d72fbe195d99147e7a11f1f3919eefd
Jake Lopez <jakedlopez@gmail.com>
2017-09-27 13:49:06 -0400

    commentary

--- jake-lopez-hw1/myshell.c
+++ jake-lopez-hw1/myshell.c
@@ -1,3 +1,13 @@
+/**
+ * Author : Jake Lopez
+ * PID: 5820326
+ * Program Description:
+ * myshell [batchfile]
+ * 
+ * A simple interactive shell program which can execute commands inputted by the user via STDIN or through a batchfile.
+ *
+ * All work is that of my own or modified from the given myshell.c template.
+ */
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,6 +16,7 @@
 
 #define MAX_ARGS 20
 
+// largely unchanged from the given template
 int get_args(char *cmdline, char* args[])
 {
     int i =0;
@@ -25,7 +36,7 @@ int get_args(char *cmdline, char* args[])
 void execute(char *cmdline)
 {
     int pid, async;
-    char* args[MAX_ARGS] = {NULL};
+    char* args[MAX_ARGS] = {NULL}; // only change from the given template. ensures that the args to be executed are always nulled
     int nargs = get_args(cmdline, args);
 
     if(nargs <= 0)
@@ -65,16 +76,17 @@ void execute(char *cmdline)
 int main(int argc, char* argv[])
 {
     char cmdline[BUFSIZ];
+    // if we were given a batch file as an arguement
     if(argc == 2)
     {
-        // reading from file
-        char *file_name = argv[argc-1];
+        char *file_name = argv[argc-1]; // since there's exactly 2 args the last one should be the file name
         FILE* file = fopen(file_name, "r");
         if(file == NULL)
         {
             printf("File %s does not exist.\n", file_name);
             exit(1);
         }
+        // keep reading till EOF
         while(fgets(cmdline, BUFSIZ, file) != NULL) 
         {
             char *save;
@@ -89,6 +101,7 @@ int main(int argc, char* argv[])
     }
     else if(argc == 1)
     {
+        // running in interactive mode
         while(1)
         {
             printf("Jake Lopez> ");
@@ -100,6 +113,7 @@ int main(int argc, char* argv[])
                 char *save;
                 char *token = strtok_r(cmdline, ";", &save);
 
+                // tokenize by the semicolon first and then execute each subcommand
                 while(token != NULL)
                 {
                     execute(token);
@@ -107,6 +121,7 @@ int main(int argc, char* argv[])
                 }
         }
     }
+    // too many arguements
     else
     {
         fprintf(stderr, "Error: too many arguements\nUsage: myshell [batchfile]\n");
